f228ad48f45e827357d217bac57cf991f75c6bc1
anyalenzy <anya.dudnik.91@gmail.com>
2024-03-30 23:36:06 +0200

    add selectFilteredContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,15 @@
 import { useSelector } from "react-redux";
-import { selectContacts, selectNameFilter } from "../../redux/selectors";
+import { selectFilteredContacts } from "../../redux/selectors";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
-const getVisibleContacts = (contacts, filter) => {
-  if (filter.length > 0) {
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(filter.trim().toLowerCase())
-    );
-  } else {
-    return contacts;
-  }
-};
 export default function ContactList() {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-  const visibleContacts = getVisibleContacts(contacts, filter);
+  const filteredContacts = useSelector(selectFilteredContacts);
   return (
     <>
-      {visibleContacts.length !== 0 ? (
+      {filteredContacts.length !== 0 ? (
         <ul className={css.contactList}>
-          {visibleContacts.map((contact) => (
+          {filteredContacts.map((contact) => (
             <Contact key={contact.id} contact={contact} />
           ))}
         </ul>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectContacts = (state) => state.contacts.items;
 
 export const selectNameFilter = (state) => state.filters.name;
@@ -5,3 +7,16 @@ export const selectNameFilter = (state) => state.filters.name;
 export const getError = (state) => state.contacts.error;
 
 export const getIsLoading = (state) => state.contacts.isLoading;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, filter) => {
+    if (filter.length > 0) {
+      return contacts.filter(({ name }) =>
+        name.toLowerCase().includes(filter.trim().toLowerCase())
+      );
+    } else {
+      return contacts;
+    }
+  }
+);
